44d6afdf98aee6dc09fb99fd39975a0058740043
Tomas <tomas.vicente.7e5@itb.cat>
2024-01-18 17:01:36 +0100

    closes #1 menu terminado

---
+++ Control/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Control
+{
+    public class Check
+    {
+        public const string StatsFail = "Has fallado 3 veces creando la stat y se te ha asignado los valores mínimos de la stat";
+        public static bool MenuChoiceInput(int menuChoice, ref int menuTries)
+        {
+            const string MenuWrongChoice = "Elegiste un valor no permitido prueba otra vez, te quedan estos intentos: {0} ";
+            const string Bye = "Hasta pronto! :D ";
+            const string Play = "Perfecto, vamos a jugar!";
+
+            if (menuChoice == 0)
+            {
+                Console.WriteLine(Bye);
+                return true;
+            }
+            else if (menuChoice == 1)
+            {
+                Console.WriteLine(Play);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(MenuWrongChoice, menuTries - 1);
+                menuTries--;
+                return menuTries > 0;
+            }
+        }
+        public static bool MenuNoTries()
+        {
+            const string MenuOutOftries = "Te quedaste sin intentos en un menú, madre mía...";
+
+            Console.WriteLine(MenuOutOftries);
+
+            return true;
+        }
+    }
+}
